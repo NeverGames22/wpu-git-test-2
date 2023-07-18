f5153cda556e3df855282db952f334781c415724
NeverGames22 <muhammad.apriadi2222@gmail.com>
2023-07-18 11:33:13 +0800

    menambahkan list

--- Index.php
+++ Index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quas ullam nam cupiditate quod nostrum animi consequuntur ipsa iusto ducimus maiores quisquam molestiae, autem mollitia ab repellat rem ea beatae?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
